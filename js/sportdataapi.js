055e1389b0dd2f6eb9511113ed87c7f048475d9d
DannyNagyAI <danny.nagy@icloud.com>
2021-03-25 00:14:59 +0000

    updated js-code, added some responsive table from boostrap, started building on more interactive solutions for the page and some design changes in CSS

--- js/sportdataapi.js
+++ js/sportdataapi.js
@@ -5,10 +5,8 @@ var SEASONS_URI = "/api/v1/soccer/seasons";
 var MATCHES_URI = "/api/v1/soccer/matches";
 var PL_LEAGUID = 237;
 var LL_LEAGUID = 538;
-
-var DATE_FROM = "2021-04-01";
-var DATE_TO = "2021-04-31";
-var pl_seasonID, ll_seasonID, current_season_id;
+var todays_date = (new Date()).toISOString().split('T')[0];
+var pl_seasonID, ll_seasonID, current_season_id, pl_end_date, ll_end_date, league_end_date;
 
 // 1. Init
 getMatches(null);
@@ -26,9 +24,13 @@ function getMatches(src) {
         // 1. Get some ID:s for current seasons ... future proof concept, gets the active season ID
         pl_seasonID = getCurrentSeasonID(data[0]);
         ll_seasonID = getCurrentSeasonID(data[1]);
+        pl_end_date = getSeasonEndDate(data[0]);
+        ll_end_date = getSeasonEndDate(data[1]);
+
         if(src === null) {
             // Set the default league to PL
             current_season_id = pl_seasonID;
+            league_end_date = pl_end_date;
             document.getElementById("pl").checked = true;
         }
         else {
@@ -36,14 +38,16 @@ function getMatches(src) {
             console.log(src.value);
             if(src.value == PL_LEAGUID) {
                 current_season_id = pl_seasonID;
+                league_end_date = pl_end_date;
             }
             else if(src.value == LL_LEAGUID) {
                 current_season_id = ll_seasonID;
+                league_end_date = ll_end_date;
             }
         }
 
         // 3. Get the matches for the current season of the chosen league
-        var url = REST_HOST+MATCHES_URI+"?"+API_KEY+"&season_id="+current_season_id+"&date_from="+DATE_FROM+"&date_to="+DATE_TO;
+        var url = REST_HOST+MATCHES_URI+"?"+API_KEY+"&season_id="+current_season_id+"&date_from="+todays_date+"&date_to="+league_end_date;
         var matches = fetch(url)
             .then(async (response) => {
             const data = await response.json();
@@ -73,22 +77,39 @@ function getCurrentSeasonID(json) {
     return current_season_id;
 }
 
+// Helper function, returns current, active season end date
+function getSeasonEndDate(json) {
+	// 1. Iterate to get the currenct active season 
+    var league_end_date = "";
+
+    json.data.forEach((season) => {
+        var end_date = JSON.parse(JSON.stringify(season["end_date"]));
+        var current_season = JSON.parse(JSON.stringify(season["is_current"])); 
+
+        if(current_season == 1) {
+            league_end_date = end_date;
+        }
+    })
+    return league_end_date;
+}
+
 /* Finally - we get to render some data - phew .. */
 function renderMatches(data) {
-    console.log(data);
-    var rownr = 1;
+    //console.log(data);
+    //var rownr = 1;
     var tablebody = document.getElementById("tablebody");
     tablebody.innerHTML = "";
 
     // iterate the games from the current season of the chosen league ..
     data.data.forEach((match) => {
-        var startDate = JSON.parse(JSON.stringify(match["match_start"])).substring(0,10)
-        var homeTeam  = JSON.parse(JSON.stringify(match["home_team"]["name"]))
-        var awayTeam  = JSON.parse(JSON.stringify(match["away_team"]["name"]))
-        var htm_logo  = JSON.parse(JSON.stringify(match["home_team"]["logo"]))
-        var atm_logo  = JSON.parse(JSON.stringify(match["away_team"]["logo"]))
+        var match_id  = JSON.parse(JSON.stringify(match["match_id"]));
+        var startDate = JSON.parse(JSON.stringify(match["match_start"])).substring(0,10);
+        var homeTeam  = JSON.parse(JSON.stringify(match["home_team"]["name"]));
+        var awayTeam  = JSON.parse(JSON.stringify(match["away_team"]["name"]));
+        var htm_logo  = JSON.parse(JSON.stringify(match["home_team"]["logo"]));
+        var atm_logo  = JSON.parse(JSON.stringify(match["away_team"]["logo"]));
         var arena     = JSON.parse(JSON.stringify(match["venue"])) === null ? "N/A" : JSON.parse(JSON.stringify(match["venue"]["name"]));
-        var city      = JSON.parse(JSON.stringify(match["venue"])) === null ? "N/A" : JSON.parse(JSON.stringify(match["venue"]["city"]));
+        //var city      = JSON.parse(JSON.stringify(match["venue"])) === null ? "N/A" : JSON.parse(JSON.stringify(match["venue"]["city"]));
 
         // Create an empty <tr> element and add it to the 1st position of the table:
         var row = tablebody.insertRow(-1);
@@ -98,18 +119,14 @@ function renderMatches(data) {
         var cell2 = row.insertCell(1);
         var cell3 = row.insertCell(2);
         var cell4 = row.insertCell(3);
-        var cell5 = row.insertCell(4);
-        var cell6 = row.insertCell(5);
 
         // Add some text to the new cells:
-        cell1.innerHTML = rownr;
-        cell2.innerHTML = startDate;
-        cell3.innerHTML = '<img src="'+htm_logo+'">'+homeTeam;
-        cell4.innerHTML = '<img src="'+atm_logo+'">'+awayTeam;
-        cell5.innerHTML = arena;
-        cell6.innerHTML = city;
+        cell1.innerHTML = startDate;
+        cell2.innerHTML = '<img src="'+htm_logo+'">'+homeTeam;
+        cell3.innerHTML = '<img src="'+atm_logo+'">'+awayTeam;
+        cell4.innerHTML = arena;
 
-        rownr ++;
+        //rownr ++;
     });
 }
 
